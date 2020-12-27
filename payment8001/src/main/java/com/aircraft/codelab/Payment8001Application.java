e83c209cd637a61e051d3a018fdd363d921dba58
spike-x <zt_wmail500@163.com>
2020-12-27 23:57:02 +0800

    init

---
+++ payment8001/src/main/java/com/aircraft/codelab/Payment8001Application.java
@@ -0,0 +1,20 @@
+package com.aircraft.codelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 2020-12-27
+ * 启动类
+ *
+ * @author tao.zhang
+ * @since 1.0
+ */
+@SpringBootApplication
+public class Payment8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001Application.class, args);
+    }
+
+}
