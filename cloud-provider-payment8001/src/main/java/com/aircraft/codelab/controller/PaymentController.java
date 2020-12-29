ad43141c881f90337a19ccf998e350053f8d45ee
spike-x <zt_wmail500@163.com>
2020-12-30 00:00:15 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/aircraft/codelab/controller/PaymentController.java
@@ -0,0 +1,16 @@
+package com.aircraft.codelab.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 2020-12-29
+ * Payment
+ *
+ * @author tao.zhang
+ * @since 1.0
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+}
