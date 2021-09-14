935f7569c3fe9d103164356d52e57e206b4b096b
Spike-X <zt_wmail500@163.com>
2021-09-15 00:23:16 +0800

    update

---
+++ cloud-config-client-3355/src/main/java/com/aircraft/codelab/config/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.aircraft.codelab.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 2021-09-14
+ *
+ * @author tao.zhang
+ * @since 1.0
+ */
+@RestController
+public class ConfigClientController {
+//    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping(value = "/configInfo", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
