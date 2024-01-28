0a49ac00b49ba59c27508c691004364d1432aa01
tao <zt_wmail500@163.com>
2024-01-28 21:00:26 +0800

    openfeign文件上传

---
+++ cloud-consumer-feign-order80/src/main/java/com/aircraft/codelab/order/config/InMemoryMultipartFile.java
@@ -0,0 +1,73 @@
+package com.aircraft.codelab.order.config;
+
+import org.springframework.lang.NonNull;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+/**
+ * 2024-01-28
+ *
+ * @author tao.zhang
+ * @since 1.0
+ */
+public class InMemoryMultipartFile implements MultipartFile {
+    private final String name;
+    private final String originalFilename;
+    private final String contentType;
+    private final byte[] content;
+
+    public InMemoryMultipartFile(String name, String originalFilename, String contentType, byte[] content) {
+        this.name = name;
+        this.originalFilename = originalFilename;
+        this.contentType = contentType;
+        this.content = content;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return originalFilename;
+    }
+
+    @Override
+    public String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return content.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return content.length;
+    }
+
+    @NonNull
+    @Override
+    public byte[] getBytes() {
+        return content;
+    }
+
+    @NonNull
+    @Override
+    public InputStream getInputStream() {
+        return new ByteArrayInputStream(content);
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+        Files.write(dest.toPath(), content);
+    }
+}
--- cloud-consumer-feign-order80/src/main/java/com/aircraft/codelab/order/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/aircraft/codelab/order/service/PaymentFeignService.java
@@ -3,9 +3,10 @@ package com.aircraft.codelab.order.service;
 import com.aircraft.codelab.core.entities.CommonResult;
 import com.aircraft.codelab.core.entities.Payment;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 2021-09-07
@@ -21,4 +22,10 @@ public interface PaymentFeignService {
 
     @GetMapping(value = "/payment/feign/timeout")
     String paymentFeignTimeout();
+
+    @RequestMapping(value = "/payment/feign/multipart", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    String multipart(@RequestPart("file") MultipartFile file);
+
+    @RequestMapping(value = "/payment/feign/multipart2", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    String multipart2(@RequestPart("file") MultipartFile file, @RequestParam("param") String param);
 }
